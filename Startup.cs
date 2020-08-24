7ac3d9474b60206a122db7d9af20a6dcdcc0d637
Seichirino <likeanopenwindow@gmail.com>
2020-08-25 00:46:32 +0300

    Use exception pages only in dev

--- Startup.cs
+++ Startup.cs
@@ -106,15 +106,15 @@ namespace BugTracker
         /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseStatusCodePagesWithRedirects("/Error/{0}");
-            //    app.UseHsts();
-            //}
+            }
+            else
+            {
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+                app.UseHsts();
+            }
             var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
             app.UseRequestLocalization(options.Value);
 
